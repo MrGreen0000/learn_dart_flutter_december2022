74a84677fabd08954d8976c49297de2fc86729bc
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-01-05 15:52:57 +0100

    learn detection plateforme flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,109 @@
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const CupertinoTabBarApp());
+  runApp(const MyApp());
 }
 
-class CupertinoTabBarApp extends StatelessWidget {
-  const CupertinoTabBarApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
+    return MaterialApp(
       title: 'Flutter Demo',
-      theme: CupertinoThemeData(brightness: Brightness.light),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(title: 'Détection de la plateforme'),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
+    return Platform.isAndroid ? material() : cupertino();
+  }
+
+  Widget material() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [Text('style Material Design')],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+            label: 'Accueil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.supervised_user_circle,
+              color: Colors.white,
+            ),
+            label: 'Mon compte',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.assessment,
+              color: Colors.white,
+            ),
+            label: 'Statistiques',
+          ),
+        ],
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+
+  Widget cupertino() {
     return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(
-        leading: Icon(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'style Cupertino',
+              style: TextStyle(fontSize: 20.0, decoration: TextDecoration.none),
+            ),
+          ],
+        ),
+      ),
+      navigationBar: CupertinoNavigationBar(
+        leading: const Icon(
           Icons.arrow_back_ios,
           color: Colors.white,
         ),
         automaticallyImplyLeading: true,
         middle: Text(
-          'Titre de la page',
-          style: TextStyle(color: Colors.white),
+          widget.title,
+          style: const TextStyle(color: Colors.white),
         ),
-        trailing: Icon(Icons.add_shopping_cart),
+        trailing: const Icon(
+          Icons.add_shopping_cart,
+          color: Colors.white,
+        ),
         backgroundColor: Colors.red,
-      ),
-      child: CupertinoTabScaffold(
-        tabBar: CupertinoTabBar(
-          backgroundColor: Colors.red,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-                icon: Icon(
-                  CupertinoIcons.home,
-                  color: Colors.white,
-                ),
-                label: ('Accueil')),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  CupertinoIcons.group,
-                  color: Colors.white,
-                ),
-                label: ('Mon compte')),
-            BottomNavigationBarItem(
-              icon: Icon(
-                CupertinoIcons.book,
-                color: Colors.white,
-              ),
-              label: ('Statistiques'),
-            ),
-          ],
-        ),
-        tabBuilder: (context, index) {
-          switch (index) {
-            case 0:
-              return CupertinoTabView(builder: (context) {
-                return CupertinoPageScaffold(
-                    backgroundColor: Colors.blue,
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            '$index: Accueil',
-                            style: const TextStyle(fontSize: 40.0),
-                          )
-                        ],
-                      ),
-                    ));
-              });
-            case 1:
-              return CupertinoTabView(builder: (context) {
-                return CupertinoPageScaffold(
-                    backgroundColor: Colors.green,
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            '$index: Mon compte',
-                            style: const TextStyle(fontSize: 40.0),
-                          ),
-                        ],
-                      ),
-                    ));
-              });
-            case 2:
-              return CupertinoTabView(builder: (context) {
-                return CupertinoPageScaffold(
-                    backgroundColor: Colors.amber,
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('$index: Statistiques',
-                              style: const TextStyle(fontSize: 40.0))
-                        ],
-                      ),
-                    ));
-              });
-          }
-          throw '';
-        },
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:sixth_app_mobile_december2022/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const CupertinoTabBarApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
