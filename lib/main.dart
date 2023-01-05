feb228a0c2504c915bc1f3e37bdeae640080092e
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-01-05 09:23:00 +0100

    learn Cipertino on flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,177 +1,122 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CupertinoTabBarApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CupertinoTabBarApp extends StatelessWidget {
+  const CupertinoTabBarApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const CupertinoApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: CupertinoThemeData(brightness: Brightness.light),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // Icon _coeur = Icon(
-  //   Icons.favorite_border,
-  //   color: Colors.white,
-  // );
-  int _indexSelectionne = 0;
-  String _affichage = "0: Accueil";
-
-  void _itemClique(int index) {
-    setState(() {
-      _indexSelectionne = index;
-      switch (_indexSelectionne) {
-        case 0:
-          {
-            _affichage = '$_indexSelectionne: Accueil';
-          }
-          break;
-        case 1:
-          {
-            _affichage = '$_indexSelectionne: Mon compte';
-          }
-          break;
-        case 2:
-          {
-            _affichage = '$_indexSelectionne: Statistique';
-          }
-          break;
-      }
-    });
-  }
-
-  // bool _likeBool = false;
-
-  // int _counter = 10;
-  // String _phrase = "Clic avant l'explosion";
-  // TextStyle _tS = const TextStyle(
-  //   color: Colors.green,
-  //   fontSize: 15.0,
-  // );
-  // Color _couleur = Colors.white;
-
-  // void _decrementCounter() {
-  //   setState(() {
-  //     if (_counter > 0) {
-  //       _counter--;
-  //     }
-
-  //     if (_counter == 0) {
-  //       _phrase = "BOOM";
-  //       _tS = const TextStyle(
-  //         color: Colors.red,
-  //         fontSize: 30.0,
-  //         fontWeight: FontWeight.bold,
-  //       );
-  //       _couleur = Colors.amber;
-  //     }
-  //   });
-  // }
-
-  // void _likeThis() {
-  //   setState(() {
-  //     if (_likeBool) {
-  //       // ignore: prefer_const_constructors
-  //       _coeur = Icon(
-  //         Icons.favorite_border,
-  //         color: Colors.white,
-  //       );
-  //       _likeBool = false;
-  //     } else {
-  //       // ignore: prefer_const_constructors
-  //       _coeur = Icon(
-  //         Icons.favorite,
-  //         color: Colors.white,
-  //       );
-  //     }
-  //   });
-  // }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          leading: IconButton(onPressed: null, icon: Icon(Icons.menu_outlined)),
-          // actions: <Widget>[IconButton(onPressed: _likeThis, icon: _coeur)],
-          // backgroundColor: Colors.red,
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        leading: Icon(
+          Icons.arrow_back_ios,
+          color: Colors.white,
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              // Text(
-              //   _phrase,
-              //   style: _tS,
-              // ),
-              // Text(
-              //   '$_counter',
-              //   style: Theme.of(context).textTheme.headline4,
-              // ),
-              Text(_affichage)
-            ],
-          ),
+        automaticallyImplyLeading: true,
+        middle: Text(
+          'Titre de la page',
+          style: TextStyle(color: Colors.white),
         ),
-        // backgroundColor: _couleur,
-        // floatingActionButton: FloatingActionButton(
-        //     onPressed: _likeThis, backgroundColor: Colors.red, child: _coeur),
-        // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-
-        // floatingActionButton: FloatingActionButton(
-        //   onPressed: _decrementCounter,
-        //   tooltip: 'Compte à rebours',
-        //   child: const Icon(Icons.history),
-        // ),
-        bottomNavigationBar: BottomNavigationBar(
+        trailing: Icon(Icons.add_shopping_cart),
+        backgroundColor: Colors.red,
+      ),
+      child: CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+          backgroundColor: Colors.red,
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home,
-                color: Colors.white,
-              ),
-              label: 'Accueil',
-            ),
+                icon: Icon(
+                  CupertinoIcons.home,
+                  color: Colors.white,
+                ),
+                label: ('Accueil')),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  CupertinoIcons.group,
+                  color: Colors.white,
+                ),
+                label: ('Mon compte')),
             BottomNavigationBarItem(
               icon: Icon(
-                Icons.supervised_user_circle,
+                CupertinoIcons.book,
                 color: Colors.white,
               ),
-              label: 'Mon compte',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.assessment,
-                color: Colors.white,
-              ),
-              label: 'Statistiques',
+              label: ('Statistiques'),
             ),
           ],
-          backgroundColor: Colors.blue,
-          onTap: _itemClique,
-          selectedItemColor: Colors.amber[800],
-          iconSize: 40.0,
-          selectedFontSize: 20.0,
-          currentIndex: _indexSelectionne,
-        ));
+        ),
+        tabBuilder: (context, index) {
+          switch (index) {
+            case 0:
+              return CupertinoTabView(builder: (context) {
+                return CupertinoPageScaffold(
+                    backgroundColor: Colors.blue,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            '$index: Accueil',
+                            style: const TextStyle(fontSize: 40.0),
+                          )
+                        ],
+                      ),
+                    ));
+              });
+            case 1:
+              return CupertinoTabView(builder: (context) {
+                return CupertinoPageScaffold(
+                    backgroundColor: Colors.green,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            '$index: Mon compte',
+                            style: const TextStyle(fontSize: 40.0),
+                          ),
+                        ],
+                      ),
+                    ));
+              });
+            case 2:
+              return CupertinoTabView(builder: (context) {
+                return CupertinoPageScaffold(
+                    backgroundColor: Colors.amber,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('$index: Statistiques',
+                              style: const TextStyle(fontSize: 40.0))
+                        ],
+                      ),
+                    ));
+              });
+          }
+          throw '';
+        },
+      ),
+    );
   }
 }
